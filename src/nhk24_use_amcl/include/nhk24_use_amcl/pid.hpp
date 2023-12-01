bb600f525b28723aba3da1ed86c695fe6cfa15ac
Stew-000-1-0-011 <stew00010011@gmail.com>
2023-12-01 21:27:20 +0900

    pacman用

---
+++ src/nhk24_use_amcl/include/nhk24_use_amcl/pid.hpp
@@ -0,0 +1,43 @@
+#pragma once
+
+namespace nhk24_use_amcl::stew::pid {
+
+	template<class T>
+	concept arithmetic = requires(T a, T b) {
+		{a + b} -> std::convertible_to<T>;
+		{a - b} -> std::convertible_to<T>;
+		{a * b} -> std::convertible_to<T>;
+		{a / b} -> std::convertible_to<T>;
+		{-a} -> std::convertible_to<T>;
+	};
+
+	template<class Scalar, class Vector>
+	concept linear_algebra = arithmetic<Scalar>
+	&& arithmetic<Vector>
+	&& requires (Scalar s, Vector v) {
+		{v * s} -> std::convertible_to<Vector>;
+		{s * v} -> std::convertible_to<Vector>;
+		{v / s} -> std::convertible_to<Vector>;
+	};
+
+	template<class ValueT, class GainT = ValueT>
+	requires linear_algebra<ValueT, GainT>
+	struct Pid final {
+		GainT k_p{1};
+		GainT k_i{0};
+		GainT k_d{0};
+
+		ValueT integral{};
+		ValueT derivative{};
+
+		static auto make(GainT k_p, GainT k_i, GainT k_d) {
+			return Pid{k_p, k_i, k_d};
+		}
+
+		auto update(ValueT error, GainT dt) noexcept {
+			integral = integral + error * dt;
+			derivative = (error - derivative) / dt;
+			return k_p * error + k_i * integral + k_d * derivative;
+		}
+	};
+}
