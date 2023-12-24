c7a275442c59c10d2503c4fa9e5d37f3931b7928
Stew-000-1-0-011 <stew00010011@gmail.com>
2023-12-24 18:16:49 +0900

    can_plugins2は今使っってない

--- src/nhk24_use_amcl/launch/ell.bash
+++ src/nhk24_use_amcl/launch/ell.bash
@@ -1,4 +1,5 @@
 # create tab
 run_command "rviz2" "rviz2"
+sleep 1
 . src/nhk24_use_amcl/launch/emcl.bash
-run_command "can_plugins2" "ros2 launch src/can_plugins2/launch/slcan_bridge_launch.xml"
+# run_command "can_plugins2" "ros2 launch src/can_plugins2/launch/slcan_bridge_launch.xml"
