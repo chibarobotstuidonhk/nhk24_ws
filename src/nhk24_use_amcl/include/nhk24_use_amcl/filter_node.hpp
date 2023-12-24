51526a02d200940f0c421c15a78df57659fe6327
Stew-000-1-0-011 <stew00010011@gmail.com>
2023-12-24 16:38:38 +0900

    不要なコメントアウトを削除

--- src/nhk24_use_amcl/include/nhk24_use_amcl/filter_node.hpp
+++ src/nhk24_use_amcl/include/nhk24_use_amcl/filter_node.hpp
@@ -53,53 +53,6 @@ namespace nhk24_use_amcl::stew::filter_node::impl {
 		private:
 		void scan_callback(const sensor_msgs::msg::LaserScan::SharedPtr msg)
 		{
-			// struct RTheta final {
-			// 	float r;
-			// 	float theta;
-
-			// 	constexpr auto to_xy() const noexcept -> stew::vec2d::Vec2d
-			// 	{
-			// 		return {r * std::cos(theta), r * std::sin(theta)};
-			// 	}
-			// };
-
-			// constexpr auto get_r_theta = [](const sensor_msgs::msg::LaserScan& msg, const u32 i) noexcept -> RTheta
-			// {
-			// 	return {msg.ranges[i], msg.angle_min + msg.angle_increment * i};
-			// };
-
-			// // remove the points inside the robot
-			// constexpr auto exclude_square = [](const RTheta& p) noexcept -> bool
-			// {
-			// 	constexpr float footprint_size = 0.700f / 1.4142f;  // 正方形な機体の1辺の半分[m]
-			// 	constexpr stew::vec2d::Vec2d base_to_lidar = {0.010, -0.040};  // ほんとはbase_linkから取得すべき
-
-			// 	const auto [r_x, r_y] = p.to_xy() - base_to_lidar;
-			// 	if(r_x * r_x < footprint_size * footprint_size && r_y * r_y < footprint_size * footprint_size) return true;
-
-			// 	return false;
-			// };
-			
-			// constexpr auto exclude_radial_gap = [](const RTheta& p, const RTheta& p_last) noexcept -> bool
-			// {
-			// 	constexpr float threshold = 0.100f;
-
-			// 	const auto gap = (p.r - p_last.r) * 2 / (p.r + p_last.r);
-			// 	return gap * gap > threshold * threshold;
-			// };
-
-			// RTheta p_last = get_r_theta(filtered_msg, 0);
-			// for(size_t i = 0; i < filtered_msg.ranges.size(); ++i)
-			// {
-			// 	const auto p = get_r_theta(filtered_msg, i);
-			// 	if(exclude_square(p) || exclude_radial_gap(p, p_last))
-			// 	{
-			// 		// filtered_msg.ranges[i] = std::nanf("");
-			// 		filtered_msg.ranges[i] = 0.0f;
-			// 	}
-			// 	p_last = p;
-			// }
-
 			auto[scan, filtered_msg] = Scan::from_msg(std::move(*msg));
 
 			const auto chain = filter_chain(
