bcab1cdf946c00184e1962e6bae248ff917f1727
Stew-000-1-0-011 <stew00010011@gmail.com>
2023-12-26 22:42:41 +0900

    修正 & 初期位置を与えるよう変更

--- src/nhk24_use_amcl/include/nhk24_use_amcl/genbacat_node.hpp
+++ src/nhk24_use_amcl/include/nhk24_use_amcl/genbacat_node.hpp
@@ -1,4 +1,6 @@
 #pragma once
+#include <chrono>
+#include <utility>
 
 #include <rclcpp/rclcpp.hpp>
 #include<tf2_ros/transform_broadcaster.h>
@@ -7,8 +9,10 @@
 
 #include "std_type.hpp"
 #include "twist2d.hpp"
+#include "geometry_msgs_convertor.hpp"
 
 namespace nhk24_use_amcl::stew::genbacat_node::impl {
+	using namespace std::chrono_literals;
 	using namespace crs_lib::integer_types;
 	using twist2d::Twist2d;
 
@@ -18,11 +22,12 @@ namespace nhk24_use_amcl::stew::genbacat_node::impl {
 		tf2_ros::TransformBroadcaster tf2_broadcaster;
 		rclcpp::Subscription<geometry_msgs::msg::Twist>::SharedPtr actual_cmd_vel_sub;
 		rclcpp::Subscription<geometry_msgs::msg::Twist>::SharedPtr reset_current_twist_sub;
+		rclcpp::TimerBase::SharedPtr timer;
 
 		GenbacatNode(const rclcpp::NodeOptions& options = rclcpp::NodeOptions{})
 			: rclcpp::Node("genbacat_node", options)
 			, tf2_broadcaster{*this}
-			, actual_cmd_vel_sub{create_subscription<geometry_msgs::msg::Twist> (
+			, actual_cmd_vel_sub{this->create_subscription<geometry_msgs::msg::Twist> (
 				"actual_cmd_vel"
 				, 1
 				, [this](const geometry_msgs::msg::Twist::SharedPtr msg) {
@@ -30,13 +35,28 @@ namespace nhk24_use_amcl::stew::genbacat_node::impl {
 					this->current_twist += {rot(vel.linear, this->current_twist.angular), vel.angular};
 				}
 			)}
-			, reset_current_twist_sub{create_subscription<geometry_msgs::msg::Twist> (
+			, reset_current_twist_sub{this->create_subscription<geometry_msgs::msg::Twist> (
 				"reset_current_twist"
 				, 10
 				, [this](const geometry_msgs::msg::Twist::SharedPtr msg) {
 					this->current_twist = Twist2d::from_msg<geometry_msgs::msg::Twist>(std::move(*msg));
 				}
 			)}
+			, timer{this->create_wall_timer (
+				1ms
+				, [this]() {
+					geometry_msgs::msg::TransformStamped msg{};
+					msg.header.frame_id = "odom";
+					msg.header.stamp = this->now();
+					msg.child_frame_id = "base_link";
+					msg.transform.translation.x = current_twist.linear.x;
+					msg.transform.translation.y = current_twist.linear.y;
+					msg.transform.translation.z = 0.0;
+					tf2::Quaternion q{};
+					q.setEuler(current_twist.angular, 0.0, 0.0);
+					msg.transform.rotation = geometry_msgs_convertor::MsgConvertor<tf2::Quaternion, geometry_msgs::msg::Quaternion>::toMsg(q);
+					this->tf2_broadcaster.sendTransform(std::move(msg));
+			})}
 		{}
 	};
 }
