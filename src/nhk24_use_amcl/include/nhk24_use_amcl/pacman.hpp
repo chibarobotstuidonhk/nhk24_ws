8a448f7530d4793e8d82af5ac1ec8a90c75c4358
Stew-000-1-0-011 <stew00010011@gmail.com>
2023-12-26 20:00:34 +0900

    mcl_poseをtf2から得るよう変更

--- src/nhk24_use_amcl/include/nhk24_use_amcl/pacman.hpp
+++ src/nhk24_use_amcl/include/nhk24_use_amcl/pacman.hpp
@@ -10,6 +10,9 @@
 #include <rclcpp/rclcpp.hpp>
 #include <tf2/LinearMath/Transform.h>
 #include <tf2/LinearMath/Matrix3x3.h>
+#include <tf2/exceptions.h>
+#include <tf2_ros/buffer.h>
+#include <tf2_ros/transform_listener.h>
 
 #include <geometry_msgs/msg/twist.hpp>
 #include <geometry_msgs/msg/pose_with_covariance_stamped.hpp>
@@ -36,13 +39,14 @@ namespace nhk24_use_amcl::stew::pacman::impl {
 		uint32_t lookahead;
 		uint32_t lookback;
 		uint32_t step;
-		Twist2d current_pose;
 		u32 current_index;
+
+		tf2_ros::Buffer tf2_buffer;
+		tf2_ros::TransformListener tf2_listener;
 
 		rclcpp::Publisher<geometry_msgs::msg::Twist>::SharedPtr cmd_vel_pub;
 		rclcpp::Publisher<nhk24_use_amcl::msg::Result>::SharedPtr result_pub;
 		rclcpp::Subscription<msg::Path>::SharedPtr path_sub;
-		rclcpp::Subscription<geometry_msgs::msg::PoseWithCovarianceStamped>::SharedPtr mcl_pose_sub;
 		rclcpp::TimerBase::SharedPtr timer;
 
 		public:
@@ -53,12 +57,12 @@ namespace nhk24_use_amcl::stew::pacman::impl {
 			, lookahead{10}
 			, lookback{10}
 			, step{5}
-			, current_pose{{0.0, 0.0}, 0.0}
 			, current_index{0}
+			, tf2_buffer{this->get_clock()}
+			, tf2_listener{tf2_buffer}
 			, cmd_vel_pub(this->create_publisher<geometry_msgs::msg::Twist>("body_twist", 1))
 			, result_pub(this->create_publisher<nhk24_use_amcl::msg::Result>("result", 10))
 			, path_sub(this->create_subscription<msg::Path>("path", 10, std::bind(&PacMan::path_callback, this, std::placeholders::_1)))
-			, mcl_pose_sub(this->create_subscription<geometry_msgs::msg::PoseWithCovarianceStamped>("mcl_pose", 10, std::bind(&PacMan::mcl_pose_callback, this, std::placeholders::_1)))
 			, timer(this->create_wall_timer(1ms, std::bind(&PacMan::timer_callback, this)))
 		{
 			this->declare_parameter<double>("k_p", pid_controller.k_p);
@@ -101,34 +105,32 @@ namespace nhk24_use_amcl::stew::pacman::impl {
 			current_index = 0;
 		}
 
-		void mcl_pose_callback(const geometry_msgs::msg::PoseWithCovarianceStamped::SharedPtr msg) {
-			using Message = geometry_msgs::msg::PoseWithCovarianceStamped;
-			using Out = tf2::WithCovarianceStamped<tf2::Transform>;
+		// void mcl_pose_callback(const geometry_msgs::msg::PoseWithCovarianceStamped::SharedPtr msg) {
+		// 	using Message = geometry_msgs::msg::PoseWithCovarianceStamped;
+		// 	using Out = tf2::WithCovarianceStamped<tf2::Transform>;
 			
-			Out transform = MsgConvertor<Out, Message>::fromMsg(std::move(*msg));
-			const auto v = transform.getOrigin();
-			double roll, pitch, yaw;
-			tf2::Matrix3x3{transform.getRotation()}.getRPY(roll, pitch, yaw);
+		// 	Out transform = MsgConvertor<Out, Message>::fromMsg(std::move(*msg));
+		// 	const auto v = transform.getOrigin();
+		// 	double roll, pitch, yaw;
+		// 	tf2::Matrix3x3{transform.getRotation()}.getRPY(roll, pitch, yaw);
 			
-			current_pose = Twist2d{v.x(), v.y(), yaw};
-		}
+		// 	current_pose = Twist2d{v.x(), v.y(), yaw};
+		// }
 
 		void timer_callback() {
 			constexpr double dt = 0.001;
+			constexpr auto take_subrange = [](size_t begin, size_t end) {
+				return std::ranges::views::drop(begin) | std::ranges::views::take(end - begin);
+			};
 
 			if(!path.has_value()) return;
 
-			auto [time, path, goal_raius] = std::move(*this->path);
+			const auto& [time, path, goal_raius] = *this->path;
+			if(const auto current_pose_ = get_current_pose(); current_pose_) {
+				const auto current_pose = *current_pose_;
 
-			Twist2d target_pose;
-			if(current_index < path.size()) {
-				constexpr auto take_subrange = [](size_t begin, size_t end) {
-					return std::ranges::views::drop(begin) | std::ranges::views::take(end - begin);
-				};
-
-				// calculate target_pose
-				// とりあえず、current_indexを直接使う
-				target_pose = Twist2d::from_msg<nhk24_use_amcl::msg::Twist2d>(path[current_index]);
+				// get target_pose
+				auto target_pose = Twist2d::from_msg<nhk24_use_amcl::msg::Twist2d>(path[std::min<u32>(current_index + step, path.size() - 1)]);
 
 				// update current_index to the most closest point in looking subrange
 				constexpr auto cast_to_twist2d = std::ranges::views::transform(Twist2d::from_msg<nhk24_use_amcl::msg::Twist2d>);
@@ -147,28 +149,35 @@ namespace nhk24_use_amcl::stew::pacman::impl {
 					++i;
 				}
 				
-				current_index = most_closest_index + step;
-			}
-			else {
-				// just heading to the goal
-				target_pose = Twist2d::from_msg<nhk24_use_amcl::msg::Twist2d>(path.back());
-			}
+				// update current_index
+				current_index = most_closest_index;
 
-			// calculate target twist
-			const Twist2d target_twist = pid_controller.update(target_pose - current_pose, dt);
-			// publish cmd_vel
-			this->publish_cmd_vel(target_twist);
+				// calculate target twist
+				const Twist2d target_twist = pid_controller.update(target_pose - current_pose, dt);
+				// convert target_twist to local one
+				const auto [local_x, local_y] = rot(target_twist.linear, -current_pose.angular);
+				// publish cmd_vel
+				geometry_msgs::msg::Twist msg{};
+				msg.linear.x = local_x;
+				msg.linear.y = local_y;
+				msg.angular.z = target_twist.angular;
+				cmd_vel_pub->publish(msg);
+			}
 		}
 
-		void publish_cmd_vel(const Twist2d& global_twist) {
-			// convert global_twist to local_twist
-			const auto [local_x, local_y] = rot(global_twist.linear, -current_pose.angular);
-
-			geometry_msgs::msg::Twist msg{};
-			msg.linear.x = local_x;
-			msg.linear.y = local_y;
-			msg.angular.z = global_twist.angular;
-			cmd_vel_pub->publish(msg);
+		auto get_current_pose() const -> std::optional<Twist2d> {
+			try {
+				const auto transform = tf2_buffer.lookupTransform("map", "base_link", tf2::TimePointZero).transform;
+				const auto v = transform.translation;
+				double roll, pitch, yaw;
+				tf2::Matrix3x3{geometry_msgs_convertor::MsgConvertor<tf2::Quaternion, geometry_msgs::msg::Quaternion>::fromMsg(transform.rotation)}
+					.getRPY(roll, pitch, yaw);
+				return Twist2d{v.x, v.y, yaw};
+			
+			} catch(const tf2::TransformException& e) {
+				RCLCPP_ERROR(this->get_logger(), "pacman:  %s", e.what());
+				return std::nullopt;
+			}
 		}
 	};
 }
