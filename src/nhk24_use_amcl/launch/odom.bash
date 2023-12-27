5c5d43f24021a4ceba87a0dde598a77bcf41eb78
Stew-000-1-0-011 <stew00010011@gmail.com>
2023-12-27 13:15:56 +0900

    .

--- src/nhk24_use_amcl/launch/odom.bash
+++ src/nhk24_use_amcl/launch/odom.bash
@@ -2,3 +2,4 @@
 run_command "rviz2" "rviz2"
 run_command "odometry2024" "ros2 run odometry2024 odom_node"
 run_command "can_plugins2" "ros2 launch src/can_plugins2/launch/slcan_bridge_launch.xml"
+# run_command "tf2_ros" "ros2 run tf2_ros tf2_echo odom base_link"
