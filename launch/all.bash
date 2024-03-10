e2f5ac6724248a0b56e3e60cef0d8d9e9c77aa3c
Stew-000-1-0-011 <stew00010011@gmail.com>
2024-03-10 22:19:06 +0900

    filter_nodeを追加

--- launch/all.bash
+++ launch/all.bash
@@ -2,6 +2,7 @@
 run_command "rviz2" "rviz2"
 . launch/around_amcl.bash
 run_command "can_plugins2" "ros2 launch src/can_plugins2/launch/slcan_bridge_launch.xml"
+run_command "filter_node" "ros2 run r2 filter_node"
 run_command "r2_node" "ros2 run r2 r2_node"
 run_command "joy" "ros2 run joy joy_node"
 
