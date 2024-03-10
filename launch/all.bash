956917f08c9253b3ce2659b86128a20d2bbab2ad
Stew-000-1-0-011 <stew00010011@gmail.com>
2024-03-10 19:25:38 +0900

    joy忘れてた

--- launch/all.bash
+++ launch/all.bash
@@ -3,6 +3,7 @@ run_command "rviz2" "rviz2"
 . launch/around_amcl.bash
 run_command "can_plugins2" "ros2 launch src/can_plugins2/launch/slcan_bridge_launch.xml"
 run_command "r2_node" "ros2 run r2 r2_node"
+run_command "joy" "ros2 run joy joy_node"
 
 # lifecycle manage
 # urg_node2 automatically transit to active by default.
